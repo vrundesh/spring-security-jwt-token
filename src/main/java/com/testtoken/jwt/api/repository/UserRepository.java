147cc462d2b90f8f10282f97950084da8c38f63f
vrunwagh <vrundesh.waghmare@tele2.com>
2020-12-01 23:39:51 +0100

    my testing code

---
+++ src/main/java/com/testtoken/jwt/api/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.testtoken.jwt.api.repository;
+
+import com.testtoken.jwt.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUserName(String username);
+}
---
+++ src/main/java/com/testtoken/jwt/api/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.testtoken.jwt.api.service;
+
+import com.testtoken.jwt.api.entity.User;
+import com.testtoken.jwt.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUserName(username);
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), new ArrayList<>());
+    }
+}
